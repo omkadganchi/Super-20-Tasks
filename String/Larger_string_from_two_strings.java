05a5d1c4ad70d8acec79838ec7efabff1838c25e
Om Kadganchi <114085272+omkadganchi@users.noreply.github.com>
2024-12-11 11:12:39 +0530

    Update Larger_string_from_two_strings.java

--- String/Larger_string_from_two_strings.java
+++ String/Larger_string_from_two_strings.java
@@ -4,8 +4,8 @@ public class Larger_string_from_two_strings {
 
 	public static void main(String[] args) {
 
-		String s1 = "Akanksha Khedkar";
-		String s2 = "Renuka Madhekar";
+		String s1 = "Om Kadganchi";
+		String s2 = "Ishwar Sheth";
 		
 		if(s1.length()>s2.length()) {
 			System.out.println(s1);
